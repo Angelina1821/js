648e76e1d5dc86db3a66ec494516531b1ee5bce6
Angelina1821 <angelrigay2001@gmail.com>
2025-02-23 23:07:25 +0300

    Первый коммит

---
+++ final.js
@@ -0,0 +1,195 @@
+class Weapon {
+    constructor(name, attack, durability, range) {
+        this.name = name;
+        this.attack = attack;
+        this.durability = durability;
+        this.initDurability = durability;
+        this.range = range;
+    }
+
+    takeDamage(damage) {
+        this.durability = Math.max(0, this.durability - damage);
+    }
+
+    getDamage() {
+        if (this.durability === 0) return 0;
+        return this.durability >= this.initDurability * 0.3 ? this.attack : this.attack / 2;
+    }
+
+    isBroken() {
+        return this.durability === 0;
+    }
+}
+
+class Arm extends Weapon {
+    constructor() {
+        super('Рука', 1, Infinity, 1);
+    }
+}
+
+class Bow extends Weapon {
+    constructor() {
+        super('Лук', 10, 200, 3);
+    }
+}
+
+class Sword extends Weapon {
+    constructor() {
+        super('Меч', 25, 500, 1);
+    }
+}
+
+class Knife extends Weapon {
+    constructor() {
+        super('Нож', 5, 300, 1);
+    }
+}
+
+class Staff extends Weapon {
+    constructor() {
+        super('Посох', 8, 300, 2);
+    }
+}
+
+class LongBow extends Bow {
+    constructor() {
+        super();
+        this.name = 'Длинный лук';
+        this.attack = 15;
+        this.range = 4;
+    }
+}
+
+class Axe extends Sword {
+    constructor() {
+        super();
+        this.name = 'Секира';
+        this.attack = 27;
+        this.durability = 800;
+    }
+}
+
+class StormStaff extends Staff {
+    constructor() {
+        super();
+        this.name = 'Посох Бури';
+        this.attack = 10;
+        this.range = 3;
+    }
+}
+
+class Player {
+    constructor(position, name) {
+        this.life = 100;
+        this.magic = 20;
+        this.speed = 1;
+        this.attack = 10;
+        this.agility = 5;
+        this.luck = 10;
+        this.description = 'Игрок';
+        this.weapon = new Arm();
+        this.position = position;
+        this.name = name;
+    }
+
+    getLuck() {
+        return (Math.random() * 100 + this.luck) / 100;
+    }
+
+    getDamage(distance) {
+        if (distance > this.weapon.range) return 0;
+        return (this.attack + this.weapon.getDamage()) * this.getLuck() / distance;
+    }
+
+    takeDamage(damage) {
+        this.life = Math.max(0, this.life - damage);
+    }
+
+    isDead() {
+        return this.life === 0;
+    }
+}
+
+class Warrior extends Player {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 120;
+        this.speed = 2;
+        this.attack = 10;
+        this.description = 'Воин';
+        this.weapon = new Sword();
+    }
+
+    takeDamage(damage) {
+        if (this.life < 60 && this.getLuck() > 0.8 && this.magic > 0) {
+            this.magic = Math.max(0, this.magic - damage);
+        } else {
+            super.takeDamage(damage);
+        }
+    }
+}
+
+class Archer extends Player {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 80;
+        this.magic = 35;
+        this.attack = 5;
+        this.agility = 10;
+        this.description = 'Лучник';
+        this.weapon = new Bow();
+    }
+
+    getDamage(distance) {
+        if (distance > this.weapon.range) return 0;
+        return (this.attack + this.weapon.getDamage()) * this.getLuck() * distance / this.weapon.range;
+    }
+}
+
+class Mage extends Player {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 70;
+        this.magic = 100;
+        this.attack = 5;
+        this.agility = 8;
+        this.description = 'Маг';
+        this.weapon = new Staff();
+    }
+}
+
+class Dwarf extends Warrior {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 130;
+        this.attack = 15;
+        this.luck = 20;
+        this.description = 'Гном';
+        this.weapon = new Axe();
+    }
+}
+
+class Crossbowman extends Archer {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 85;
+        this.attack = 8;
+        this.agility = 20;
+        this.luck = 15;
+        this.description = 'Арбалетчик';
+        this.weapon = new LongBow();
+    }
+}
+
+class Demiurge extends Mage {
+    constructor(position, name) {
+        super(position, name);
+        this.life = 80;
+        this.magic = 120;
+        this.attack = 6;
+        this.luck = 12;
+        this.description = 'Демиург';
+        this.weapon = new StormStaff();
+    }
+}
+ssh -T git@github.com
